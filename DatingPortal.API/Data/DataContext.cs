c0e9f59b2d0031e145093f7bbb00915ed2272a13
Lukasz <lx.zbikowski@gmail.com>
2019-06-27 21:32:04 +0200

    Create first model and add DbContext

---
+++ DatingPortal.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingPortal.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingPortal.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
