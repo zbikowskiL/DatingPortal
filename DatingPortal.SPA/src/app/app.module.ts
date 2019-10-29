9e051c22dd742131e126cf22a2b10300102e11ad
Lukasz <lx.zbikowski@gmail.com>
2019-10-29 20:03:20 +0100

    Add FileUploaderComponent and upload method

--- DatingPortal.SPA/src/app/app.module.ts
+++ DatingPortal.SPA/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { RouterModule } from '@angular/router';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { NgxGalleryModule } from 'ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -51,6 +52,7 @@ export function tokenGetter() {
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    FileUploadModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
--- DatingPortal.SPA/src/app/users/photos/photos.component.ts
+++ DatingPortal.SPA/src/app/users/photos/photos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from '../../_models/photo';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-photos',
@@ -10,9 +13,46 @@ export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[];
 
-  constructor() { }
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializerUploader();
   }
 
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializerUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+
+    this.uploader.onSuccessItem = (file, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo: Photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+
+        this.photos.push(photo);
+      }
+    };
+  }
 }
